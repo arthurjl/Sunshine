8beba9507d4cf425e0c2089450d25d254c64389f
Jake Jung <junyong217@hotmail.com>
2019-10-13 11:22:42 -0700

    Create QuestionScreen.js

---
+++ MyReactNative/src/screens/QuestionScreen.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import {
+  Text,
+  StyleSheet,
+  View,
+  Alert,
+  AsyncStorage,
+  Image,
+  Dimensions,
+  Button,
+} from 'react-native';
+
+import { LinearGradient } from 'expo-linear-gradient';
+
+export default class QuestionScreen extends React.Component {
+  state = {
+    weather: this.props.weather,
+    navigation: this.props.navigation
+  }
+
+
+  render() {
+
+    return (
+      <LinearGradient 
+        colors={this.state.weather === 'sunny' ? sunny : this.state.weather === 'cloudy' ? cloudy : thunder}
+        style={styles.container}
+      >
+        { this.state.weather === 'sunny' ? <Image source={require('../assets/Images/sun.png')}  style={styles.iconSize} /> : null}
+        { this.state.weather === 'cloudy' ? <Image source={require('../assets/Images/clouds.png')}  style={styles.iconSize} /> : null}
+        { this.state.weather === 'thunder' ? <Image source={require('../assets/Images/storm.png')}  style={styles.iconSize} /> : null}      
+        <Button
+           title="To Song"
+          onPress={() => this.state.navigation.navigate('Home')}
+        />
+      </LinearGradient>
+    );
+  }
+}
+
+const sunny = ['#7DBCDE', '#01BFFF', '#7DBCDE']
+const cloudy = ['#A0CEDC', '#DEE3E7', '#A0CEDC']
+const thunder = ['#AEAFAF', '#C2C2C2', '#C2C2C2', '#2F373F']
+
+
+const deviceWidth = Dimensions.get('window').width
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: deviceWidth,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  iconSize: {
+    width: 200,
+    height: 200,
+  },
+
+});
